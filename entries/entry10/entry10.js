791c98d32dd2342674e4aa0f7f5cf8862195e38f
tarinisinghania <singt426@newschool.edu>
2024-11-14 11:23:19 -0500

    entry 9 flexbox

---
+++ entries/entry10/entry10.js
@@ -0,0 +1,12 @@
+console.log("Hello")
+
+
+let body = document.body;
+
+function turnOnLight() {
+  body.classList.toggle("light");
+  button.classList.toggle("buttonOn");
+}
+
+let button = document.querySelector(".button");
+button.addEventListener("click", turnOnLight);
